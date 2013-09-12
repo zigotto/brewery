3d2d63a8a4db1d43bd5d729d90cb7fa623b96dbc
Jesus Lopes <jlopes@zigotto.com.br>
2013-09-11 22:37:35 -0300

    Initial code for refractometer class

--- lib/brewery/refractometer.rb
+++ lib/brewery/refractometer.rb
@@ -1,30 +1,52 @@
 module Brewery
   class Refractometer
-    attr_accessor :original_brix, :final_brix
-
-    def initialize(args)
-      @original_brix = args[:original_brix]
-      @final_brix = args[:final_brix]
+    def style(name = nil)
+      if name
+        @style = Brewery.guides(:bjcp).find(id: name)
+      else
+        g(:style)
+      end
     end
 
-    def specific_gravity
-      1.000898 + (0.003859118*original_brix) + (0.00001370735*original_brix*original_brix) + (0.00000003742517*original_brix*original_brix*original_brix)
+    def original_brix(brix)
+      @original_brix = brix
+    end
+
+    def final_brix(brix)
+      @final_brix = brix
+    end
+
+    def original_gravity
+      1.000898 + (0.003859118 * g(:original_brix)) +
+        (0.00001370735 * g(:original_brix) * g(:original_brix)) +
+        (0.00000003742517 * g(:original_brix) * g(:original_brix) * g(:original_brix))
     end
 
     def final_gravity
-      1.001843 - (0.002318474*original_brix) - (0.000007775*original_brix*original_brix) - (0.000000034*original_brix*original_brix*original_brix) + (0.00574*final_brix) + (0.00003344*final_brix*final_brix) + (0.000000086*final_brix*final_brix*final_brix) 
+      1.001843 - (0.002318474 * g(:original_brix)) -
+        (0.000007775 * g(:original_brix) * g(:original_brix)) -
+        (0.000000034 * g(:original_brix) * g(:original_brix) * g(:original_brix)) +
+        (0.00574 * g(:final_brix)) +
+        (0.00003344 * g(:final_brix) * g(:final_brix)) +
+        (0.000000086 * g(:final_brix) * g(:final_brix) * g(:final_brix))
     end
 
     def index_of_refraction
-      1.33302 + (0.001427193*final_brix) + (0.000005791157*final_brix*final_brix)
+      1.33302 + (0.001427193 * g(:final_brix)) + (0.000005791157 * g(:final_brix) * g(:final_brix))
     end
 
     def alcohol_by_weight
-      1017.5596 - (277.4*final_gravity) + index_of_refraction*((937.8135*index_of_refraction) - 1805.1228)
+      1017.5596 - (277.4 * final_gravity) + index_of_refraction * ((937.8135 * index_of_refraction) - 1805.1228)
     end
 
     def alcohol_by_volume
-      alcohol_by_weight*(final_gravity/0.794)
+      alcohol_by_weight * (final_gravity/0.794)
+    end
+
+    private
+
+    def g(name)
+      instance_variable_get("@#{name}")
     end
   end
 end
